3583fc6c8b0062a6315e345ef1a33e05b7044d12
RudnickiPawel <rudnicki.pawel94@gmail.com>
2021-09-04 21:14:19 +0200

    browser side order handling started

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,27 +3,56 @@ import OrderItem from './OrderItem/OrderItem';
 import { useState } from 'react';
 
 const Order = (props) => {
-  const [orders, setorders] = useState({});
-  const handleOrderListChange = (newOrder) => {
-    setorders({...orders, newOrder});
-    console.log('ordersAfterChange: ' + orders);
+  const [order, setOrder] = useState([]);
+  const handleNewOrder = (newOrder) => {
+    setOrder([...order, newOrder]);
+    // console.log('ordersAfterChange: ' + order);
+    handleShowNewOrder(newOrder);
   };
+  const handleShowNewOrder = (newOrder) => {
+    const name = newOrder.name;
+    const size = newOrder.size;
+    const line = name + ' ' + size + ' 1';
+    // newOrder tak wyglada:
+    // {
+    //   name: name,
+    //   size: size.value
+    // }
+    // sprawdz czy juz taki jest
+    //   jak jest to + 1
+    //   jak nie ma to zrob
+    //     fetch cenę z bazy i też pokaż(czy raczej fetch wszystkie ceny na początku?)
+
+    // if (document.querySelectorAll())
+    let newOrderLine = document.createElement('p');
+    // newOrderLine.id = 'content';
+    newOrderLine.className = name + '__' + size;
+    newOrderLine.innerText = line;
+
+    document.getElementsByClassName('Order__selected')[0].appendChild(newOrderLine);
+  };
+
   return (
     <div className='Order section'>
       <div className='section__title'>Zamawianie</div>
-      <div className='Order__container'>
-        <OrderItem order={handleOrderListChange} name='margherita' />
-        <OrderItem order={handleOrderListChange} name='pepperoni' />
-        <OrderItem order={handleOrderListChange} name='hawajska' />
-        <OrderItem order={handleOrderListChange} name='carbonara' />
-        <OrderItem order={handleOrderListChange} name='vesuvio' />
-        <OrderItem order={handleOrderListChange} name='capricciosa' />
-        <OrderItem order={handleOrderListChange} name='vegetariana' />
-      </div>
+      <div className="section__paragraph">Kliknij, by dodać do listy. Kliknij zamówienie na liście, by je usunąć.</div>
       <div className='Order__list'>
         <div className="section__title">Wybrano:</div>
-        
+        <div className="Order__selected">
+          {/* {console.log([...order])} */}
+          {/* {[...orders].map(order => order)} */}
         </div>
+      </div>
+      <div className='Order__container'>
+        <OrderItem order={handleNewOrder} name='margherita' />
+        <OrderItem order={handleNewOrder} name='pepperoni' />
+        <OrderItem order={handleNewOrder} name='hawajska' />
+        <OrderItem order={handleNewOrder} name='carbonara' />
+        <OrderItem order={handleNewOrder} name='vesuvio' />
+        <OrderItem order={handleNewOrder} name='capricciosa' />
+        <OrderItem order={handleNewOrder} name='vegetariana' />
+      </div>
+
     </div>
   );
 }
--- src/components/Order/OrderItem/OrderItem.js
+++ src/components/Order/OrderItem/OrderItem.js
@@ -1,24 +1,103 @@
+// import { render } from '@testing-library/react';
+import { useState } from 'react';
+import Select from 'react-select';
 
 const OrderItem = (props) => {
-  const itemname = props.name; //all lower case
-  const itemName = itemname.charAt(0).toUpperCase() + itemname.slice(1); //first letter upper case
+  const options = [
+    { value: 30, label: '30cm' },
+    { value: 40, label: '40cm' },
+    { value: 50, label: '50cm' },
+    { value: 60, label: '60cm' },
+  ];
+
+  const customStyles = {
+    // menu: (provided, state) => ({
+    // ...provided,
+    // width: 300,
+    // }),
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: '1px dotted pink',
+      color: state.isSelected ? 'red' : 'blue',
+      padding: 20,
+      // width: 300,
+    }),
+    // control: () => ({
+    //   // width: 300,
+    // }),
+    // singleValue: (provided, state) => {
+    //   const opacity = state.isDisabled ? 0.5 : 1;
+    //   const transition = 'opacity 300ms';
+
+    //   return { ...provided, opacity, transition };
+    // }
+  }
+
+  // const handleSizeChange = (event) => {
+  //   setPrepareOrder(event.target.value);
+  // }
+  const handleChange = selectedOption => {
+    setState({ selectedOption });
+  };
+
+  const [state, setState] = useState({ selectedOption: null });
+  const { selectedOption } = state;
+  const itemname = props.name;
+  console.log(selectedOption);
+  // let output;
+  // if(selectedOption !== null )
+  //   output = selectedOption.value;
+
+  // render() {
+  // }; 
   return (
-    <div className={"OrderItem " + itemname}>
-      <div className='OrderItem__Name'>{itemName}</div>
-      <div onClick={() => handleItemValueChange(itemname, 1)} className='OrderItem__increment colorOrange'>+</div>
-      <input className='OrderItem__value' type='number' placeholder='0' />
-      <div onClick={() => handleItemValueChange(itemname, 0)} className='OrderItem__decrement colorOrange'>-</div>
+    <div className={'OrderItem ' + itemname}>
+      <div className='OrderItem__name'>{itemname.toUpperCase()}</div>
+      <Select
+        placeholder='wybierz rozmiar'
+        // styles={customStyles}
+        value={selectedOption}
+        onChange={handleChange}
+        options={options}
+      />
+      {/* {output ? <p>{output}</p> : null} */}
+      {/* <select value={state} onChange={handleSizeChange}>
+        <option value={30}>30cm</option>
+        <option value={40}>40cm</option>
+        <option value={50}>50cm</option>
+        <option value={60}>60cm</option>
+      </select> */}
+      {/* <div onClick={() => handleItemValueChange(itemname, 1, props.order)} className='OrderItem__increment colorOrange'>+</div> */}
+      {/* <input className='OrderItem__value' type='number' placeholder='0' /> */}
+      {/* <div onClick={() => handleItemValueChange(itemname, 0, props.order)} className='OrderItem__decrement colorOrange'>-</div> */}
+      <div onClick={() => { handleAddToList(props.order, selectedOption, itemname)}} className='OrderItem__send'>Dodaj</div>
     </div>
   );
 }
 
-const handleItemValueChange = (item, operation) => {
-  let output = document.querySelectorAll('.' + String(item) + ' .OrderItem__value')[0];
-  if (operation)
-    output.value = Number(output.value) + 1;
-  else if (output.value > 0)
-    output.value = Number(output.value) - 1;
-  else return;
+const handleAddToList = (order, size, name) => {
+  if(size) {
+    const output = {
+      name: name,
+      size: size.value
+    }
+    order(output);
+  }
+  else
+    alert('Wybierz rozmiar pizzy');
 };
+
+// const handleItemValueChange = (item, operation, stateUpdate) => {
+//   let output = document.querySelectorAll('.' + String(item) + ' .OrderItem__value')[0];
+//   if (operation) {
+//     output.value = Number(output.value) + 1;
+//     stateUpdate(item, output.value,);
+//   }
+//   else if (output.value > 0) {
+//     output.value = Number(output.value) - 1;
+//     stateUpdate();
+//   }
+//   else return;
+// };
 
 export default OrderItem;
