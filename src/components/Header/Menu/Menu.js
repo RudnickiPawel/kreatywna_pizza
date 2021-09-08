f5b6fd4df096fb20c380a5930994c7a0fe4a1111
RudnickiPawel <rudnicki.pawel94@gmail.com>
2021-09-08 13:17:52 +0200

    fixed header overflow

--- src/components/Header/Menu/Menu.js
+++ src/components/Header/Menu/Menu.js
@@ -4,11 +4,13 @@ import { handleLinkClick } from '../../../utils';
 const Menu = (props) => {
   return (
     <div className='Menu'>
-      <a onClick={e => {handleLinkClick(e, 'Order', props.section)}} href="/" className="Menu__link Menu__link--order">ZAMÓW</a>
-      <div className="Menu__container">
-        <a onClick={e => {handleLinkClick(e, 'Recommended', props.section)}} href="/" className="Menu__link">POLECANE</a>
-        <a onClick={e => {handleLinkClick(e, 'About', props.section)}} href="/" className="Menu__link">O NAS</a>
-        <a onClick={e => {handleLinkClick(e, 'Career', props.section)}} href="/" className="Menu__link">PRACA</a>
+      <a onClick={e => { handleLinkClick(e, 'Order', props.section) }} href="/" className="Menu__link Menu__link--order">ZAMÓW</a>
+      <div className="Menu__wrapper">
+        <div className="Menu__container">
+          <a onClick={e => { handleLinkClick(e, 'Recommended', props.section) }} href="/" className="Menu__link">POLECANE</a>
+          <a onClick={e => { handleLinkClick(e, 'About', props.section) }} href="/" className="Menu__link">O NAS</a>
+          <a onClick={e => { handleLinkClick(e, 'Career', props.section) }} href="/" className="Menu__link">PRACA</a>
+        </div>
       </div>
     </div>
   )
