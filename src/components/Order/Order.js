fae3546d0e9da252d794e4354c53d7dc4c991742
RudnickiPawel <rudnicki.pawel94@gmail.com>
2021-09-22 16:48:19 +0200

    adding checkout

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,13 +1,17 @@
 import '../../styles/main.css';
 import OrderItem from './OrderItem/OrderItem';
 import OrderCustom from './OrderCustom/OrderCustom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 
 const Order = (props) => {
   const [componentPrices, setComponentPrices] = useState(null);
   const [pizzasComponents, setPizzasComponents] = useState(null);
   const [orders, setOrders] = useState([]);
+  const [toggleModal, setToggleModal] = useState(false);
   let totalPrice = 0.00;
+  let showGoToCheckout = false;
+  if (orders[0])
+    showGoToCheckout = true;
   orders.map(order => {
     totalPrice += parseFloat(order.price);
     return null;
@@ -112,14 +116,17 @@ const Order = (props) => {
       setOrders(dummyState);
     }
   };
+  const handleToggleModal = (isModalVisible) => {
+    setToggleModal(isModalVisible);
+  };
 
   return (
     <div className='Order section'>
       <div className='section__title'>Zamawianie</div>
-      <div className="section__paragraph">Kliknij na przycisk Dodaj, by dodać do listy. <br />Kliknij zamówienie na liście, by je usunąć.</div>
+      <div className='section__paragraph'>Kliknij na przycisk Dodaj, by dodać do listy. <br />Kliknij zamówienie na liście, by je usunąć.</div>
       <div className='Order__list'>
-        <div className="section__title">Wybrano:</div>
-        <div className="Order__selected">
+        <div className='section__title'>Wybrano:</div>
+        <div className='Order__selected'>
           {
             !(orders === null) ? orders.map(order =>
               <div onClick={() => {
@@ -131,20 +138,72 @@ const Order = (props) => {
             ) : null
           }
         </div>
-        <div className="Order__price">Suma: {(Math.round(totalPrice * 100) / 100).toFixed(2)} zł</div>
+        <div className='Order__price'>Suma: {(Math.round(totalPrice * 100) / 100).toFixed(2)} zł
+          {
+            showGoToCheckout ?
+              <div className='Order__goToForm' onClick={() => {
+                const target = document.getElementsByClassName('Order__checkout')[0];
+                target.scrollIntoView();
+              }}>Zatwierdź listę</div>
+              : null
+          }
+        </div>
       </div>
       {
         pizzasComponents ?
-          <div className='Order__container'>
-            <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.margherita} componentPrices={componentPrices} name='margherita' />
-            <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.pepperoni} componentPrices={componentPrices} name='pepperoni' />
-            <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.hawajska} componentPrices={componentPrices} name='hawajska' />
-            <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.carbonara} componentPrices={componentPrices} name='carbonara' />
-            <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.vesuvio} componentPrices={componentPrices} name='vesuvio' />
-            <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.capricciosa} componentPrices={componentPrices} name='capricciosa' />
-            <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.vegetariana} componentPrices={componentPrices} name='vegetariana' />
-            <OrderCustom order={handleOrder} componentPrices={componentPrices} />
-          </div>
+          <Fragment>
+            <div className='Order__container'>
+              {showGoToCheckout ? <div className='Order__goToCheckout' onClick={() => {
+                const target = document.getElementsByClassName('Order__list')[0];
+                target.scrollIntoView();
+              }}>przejdź do listy</div> : null}
+              <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.margherita} componentPrices={componentPrices} name='margherita' />
+              <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.pepperoni} componentPrices={componentPrices} name='pepperoni' />
+              <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.hawajska} componentPrices={componentPrices} name='hawajska' />
+              <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.carbonara} componentPrices={componentPrices} name='carbonara' />
+              <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.vesuvio} componentPrices={componentPrices} name='vesuvio' />
+              <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.capricciosa} componentPrices={componentPrices} name='capricciosa' />
+              <OrderItem order={handleOrder} pizzaComponents={pizzasComponents.vegetariana} componentPrices={componentPrices} name='vegetariana' />
+              <OrderCustom order={handleOrder} componentPrices={componentPrices} />
+            </div>
+            <div className='Order__checkout'>
+              <div className="section__title">Dane do wysyłki</div>
+              <label>
+                Miasto:
+                <input type='text' className="Order__checkout-input Order__checkout_city" placeholder='miasto' />
+              </label>
+              <label>
+                Ulica:
+                <input type='text' className="Order__checkout-input Order__checkout_street" placeholder='ulica' />
+              </label>
+              <label>
+                Nr budynku:
+                <input type='text' className="Order__checkout-input Order__checkout_houseNr" placeholder='numer domu' />
+              </label>
+              <label>
+                Nr mieszkania:
+                <input type='text' className="Order__checkout-input Order__checkout_flatNr" placeholder='numer mieszkania' />
+              </label>
+              <label>
+                Nr telefonu:
+                <input type='tel' className="Order__checkout-input Order__checkout_phone" placeholder='numer telefonu' />
+              </label>
+              <div className="Order__send" onClick={() => {
+                let anyInputEmpty = false;
+                let inputsValues = document.getElementsByClassName('Order__checkout-input'); //htmlCollection
+                inputsValues = [].slice.call(inputsValues); //turning into Array
+                inputsValues = inputsValues.map(input => {
+                  if (input.value === '')
+                    anyInputEmpty = true;
+                })
+                if(anyInputEmpty)
+                  alert('Ups! Coś poszło nie tak! Upewnij się, że dane zostały wprowadzone prawidłowo.');
+                else
+                  alert('Zamówienie zostało wysłane!');
+                //normalnie w tym momencie robiona by była walidacja danych przez serwer, płatność i potem przechowanie danych w bazie danych na serwerze; ja jednak nie będę implementował tych funkcjonalności dla tego projektu; 
+              }}>Wyślij</div>
+            </div>
+          </Fragment>
           : 'Wczytywanie...'
       }
     </div >
