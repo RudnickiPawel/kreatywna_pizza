57907c022c9a6d467275588675770b1cf021215a
RudnickiPawel <rudnicki.pawel94@gmail.com>
2021-09-30 16:41:52 +0200

    career section done

--- src/components/Career/Career.js
+++ src/components/Career/Career.js
@@ -2,8 +2,14 @@ import '../../styles/main.css';
 
 const Career = (props) => {
   return (
-    <div className='Career global__section'>
-      {/* <p>{props.which}</p> */}
+    <div className='Career section'>
+      <div className="section__title">Praca</div>
+      <div className="section__paragraph">Nasza firma rozwija się bardzo dynamicznie i stale prowadzimy rekrutację nowych pracowników.</div>
+      <div className="section__paragraph">CV nie jest wymagane, ale doświadczenie w kontakcie w klientem lub w kuchni jest mile widziane.</div>
+      <div className="section__paragraph">Potrzebujemy ludzi pozytywnych, zawsze gotowych poprawić klientom humor.</div>
+      <div className="section__paragraph">Potrzebujemy ludzi pracowitych, którzy sprawią, że zamówienia będą szybko realizowane.</div>
+      <div className="section__paragraph">Otwieramy kolejne restauracje i na ich managerów wybieramy najlepszych spośród naszych pracowników. Jest tu potencjał rozwoju!</div>
+      <div className="section__paragraph">Prosimy, aby chętni pisali do nas na email podany poniżej.</div>
     </div>
   );
 }
